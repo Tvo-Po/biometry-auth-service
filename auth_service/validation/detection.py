dc3451acbb58ad7cf0c14292ce1909fd02b46138
Tvo-Po <maxwellgleb@gmail.com>
2023-05-16 04:44:36 +0300

    Liveness validation
    Also change error raising to bool return in validation

--- auth_service/validation/detection.py
+++ auth_service/validation/detection.py
@@ -1,12 +1,9 @@
-import io
 from pathlib import Path
 import cv2
 import numpy as np
 
-from exception import FaceValidationError
-    
 
-def validate_only_one_face_on_image(buffer) -> None:
+def validate_only_one_face_on_image(buffer) -> bool:
     image_array = np.asarray(buffer, dtype=np.uint8)
     image = cv2.imdecode(image_array, -1)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -15,21 +12,18 @@ def validate_only_one_face_on_image(buffer) -> None:
         'haarcascade_frontalface_default.xml'
     )
     # TODO: configure for subject domain
+    # http://repo.ssau.ru/bitstream/Informacionnye-tehnologii-i-nanotehnologii/Issledovanie-tochnosti-detektirovaniya-lic-po-izobrazheniyam-v-zavisimosti-ot-rasy-i-pola-pri-pomoshi-kaskadov-Haara-84919/1/%d0%98%d0%a2%d0%9d%d0%a2-2020_%d1%82%d0%be%d0%bc%204-481-488.pdf
     faces = face_cascade.detectMultiScale(
         gray_image,
         scaleFactor=1.3,
         minNeighbors=3,
         minSize=(30, 30),
     )
-    print(len(faces))
-    if len(faces) != 1:
-        raise FaceValidationError(
-            f"Only one face must be on input image. Found {len(faces)}."
-        )
+    return len(faces) == 1
 
 
 if __name__ == '__main__':
-    path = Path(__file__).parent.parent.parent / 'images' / 'people.jpg'
+    path = Path(__file__).parent.parent.parent / 'images' / 'pbpOZZBu0q0.png'
     with open(path, 'rb') as file:
         buffer = bytearray(file.read())
     validate_only_one_face_on_image(buffer)
---
+++ auth_service/validation/liveness.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import antispoofing
+from antispoofing.anti_spoof_predict import AntiSpoofPredict
+from antispoofing.generate_patches import CropImage
+from antispoofing.utility import parse_model_name
+import cv2
+import numpy as np
+
+
+MODEL_DIR = Path(antispoofing.__file__).parent / 'resources' / 'anti_spoof_models'
+
+
+def validate_liveness(image_path: Path) -> bool:
+    model = AntiSpoofPredict(0)
+    cropper = CropImage()
+    image = cv2.imread(image_path.as_posix())
+    image_bbox = model.get_bbox(image)
+    prediction = np.zeros((1, 3))
+    for model_name in MODEL_DIR.glob('*.pth'):
+        h_input, w_input, _, scale = parse_model_name(model_name.name)
+        param = {
+            "org_img": image,
+            "bbox": image_bbox,
+            "scale": scale,
+            "out_w": w_input,
+            "out_h": h_input,
+            "crop": True,
+        }
+        if scale is None:
+            param["crop"] = False
+        img = cropper.crop(**param)
+        prediction += model.predict(img, model_name.as_posix())
+    return np.argmax(prediction) == 1
+
+
+if __name__ == '__main__':
+    print(validate_liveness(Path('/home/tvo-po/_Content/VKR/system/images/g.png')))
