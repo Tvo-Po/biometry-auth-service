8ca3892f49cb404d5fa916e06cf89e8dc39c3a35
Tvo-Po <maxwellgleb@gmail.com>
2023-05-17 04:13:41 +0300

    Validation correct

--- auth_service/validation/detection.py
+++ auth_service/validation/detection.py
@@ -1,18 +1,14 @@
 from pathlib import Path
 import cv2
-import numpy as np
 
 
-def validate_only_one_face_on_image(buffer) -> bool:
-    image_array = np.asarray(buffer, dtype=np.uint8)
-    image = cv2.imdecode(image_array, -1)
+def validate_only_one_face_on_image(image_path: Path) -> bool:
+    image = cv2.imread(image_path.as_posix())
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades +
         'haarcascade_frontalface_default.xml'
     )
-    # TODO: configure for subject domain
-    # http://repo.ssau.ru/bitstream/Informacionnye-tehnologii-i-nanotehnologii/Issledovanie-tochnosti-detektirovaniya-lic-po-izobrazheniyam-v-zavisimosti-ot-rasy-i-pola-pri-pomoshi-kaskadov-Haara-84919/1/%d0%98%d0%a2%d0%9d%d0%a2-2020_%d1%82%d0%be%d0%bc%204-481-488.pdf
     faces = face_cascade.detectMultiScale(
         gray_image,
         scaleFactor=1.3,
@@ -20,10 +16,3 @@ def validate_only_one_face_on_image(buffer) -> bool:
         minSize=(30, 30),
     )
     return len(faces) == 1
-
-
-if __name__ == '__main__':
-    path = Path(__file__).parent.parent.parent / 'images' / 'pbpOZZBu0q0.png'
-    with open(path, 'rb') as file:
-        buffer = bytearray(file.read())
-    validate_only_one_face_on_image(buffer)
---
+++ auth_service/validation/suitability.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import numpy as np
+from tensorflow import keras
+
+from auth_service.config import settings
+
+
+MODEL = keras.models.load_model(settings.PROJECT_ROOT / 'suitability_model')
+
+
+def validate_image_suitability(image_path: Path) -> bool:
+    image = keras.utils.img_to_array(image_path.as_posix())
+    input_arr = np.array([keras.utils.img_to_array(image)])
+    return bool(round(MODEL.predict(input_arr)))
